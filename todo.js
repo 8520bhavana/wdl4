ca8862f0db9784a9a1c22a16937f292bb1b60cd9
8520bhavana <117569383+8520bhavana@users.noreply.github.com>
2022-11-20 18:28:20 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,6 +1,5 @@
-// todo.js
 const todoList = () => {
-  let all = [];
+  const all = [];
   const add = (todoItem) => {
     all.push(todoItem);
   };
@@ -25,7 +24,52 @@ const todoList = () => {
       (item) => item.dueDate > new Date().toLocaleDateString("en-CA")
     );
   };
-  return { all, add, markAsComplete, overdue, dueToday, dueLater };
+
+  const toDisplayableList = (list) => {
+    const dsl = [];
+    list.forEach((element) => {
+      if (element.dueDate === today) {
+        if (element.completed === true) {
+          const a = "[x] " + element.title;
+          dsl.push(a);
+        } else {
+          const a = "[ ] " + element.title;
+          dsl.push(a);
+        }
+      } else {
+        if (element.completed === true) {
+          const a = "[x] " + element.title + " " + element.dueDate;
+          dsl.push(a);
+        } else {
+          const a = "[ ] " + element.title + " " + element.dueDate;
+          dsl.push(a);
+        }
+      }
+    });
+    let g = "";
+    for (let i = 0; i < dsl.length; i++) {
+      // eslint-disable-next-line no-undef
+      obj = dsl[i];
+      if (i === 0) {
+        // eslint-disable-next-line no-undef
+        g = g + obj;
+      } else {
+        // eslint-disable-next-line no-undef
+        g = g + "\n" + obj;
+      }
+    }
+    return g;
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
 };
 
 module.exports = todoList;
